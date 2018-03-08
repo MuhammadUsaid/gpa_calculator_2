0e80e5135c72f65d23d423cfda48a9b1d3842c6f
MuhammadUsaid <mu04032@st.habib.edu.pk>
2018-03-08 09:08:15 +0500

    Semester Box Changing Function Is Working

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
 //Changing Semester Box
 function displayOther(id){
-    target = document.getElementById(id+"_form")
-    semesters = []
-    for(i=1;i<12;i++){
-        semesters.push(document.getElementById('sem{i}_form'))
-        id==target? hideAndSeek(semesters[i],True): hideAndSeek(semesters[i],False)
+    console.log(id)
+    target = document.getElementById(id+"form")
+    for(i=1;i<=11;i++){
+        document.getElementById('semester'+String(i)+'form').style.display = "none"
     }
-    // sem1 = document.getElementById('sem1_form')
-    // sem2 = document.getElementById('sem2_form')
-    // sem3 = document.getElementById('sem3_form')
-    // sem4 = document.getElementById('sem4_form')
-    // sem5 = document.getElementById('sem5_form')
-    // sem6 = document.getElementById('sem6_form')
-    // sem7 = document.getElementById('sem7_form')
-    // sem8 = document.getElementById('sem8_form')
-    // sum1 = document.getElementById('sem8_form')
-    // sum2 = document.getElementById('sem8_form')
-    // sum3 = document.getElementById('sem8_form')
+    target.style.display = "inline"
 }
-function hideAndSeek(id,hide){
-    hide? id.style.display="none": id.style.display="inline"
-}
+
+function addGpaToCgpa(){}
